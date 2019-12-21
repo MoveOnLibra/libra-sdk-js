6d69ef1915821c39b729065478ad2a98f46a968b
yuan_xy <yuan_xin_yu@hotmail.com>
2019-12-21 17:07:53 +0800

    split api in diffrent files by tag

--- test/client.test.js
+++ test/client.test.js
@@ -4,15 +4,27 @@ const assert = require('assert');
 
 const appkey = "eyJhbGciOiJIUzUxMiJ9.eyJkYXRhIjoidHgxeHl4MXh1IiwiaWF0IjoxNTcyOTI0NzQxLCJleHAiOjE2MDQ0NjA3NDF9.2yh_gbH266nWHQ9E_fghs7vVoFHT7a1Z6Zi-NEYt7VTmzK8GPG7BzrBkJ3HATCoVFawss_tLMqqHRUtsGVkJSQ";
 
+describe('#debugKey()', function() {
+  this.timeout(30000);
+  it('respond with matching records', async function() {
+    var client = new LibraClient("testnet", appkey);
+    //console.log(client);
+    assert.equal(2.0, 2);
+    data = await client.authkeyAPI.debugKey();
+    assert.equal(data.token, appkey);
+    console.log(data);
+    return data;
+  });
+});
 
 describe('#get_balance()', function() {
   this.timeout(15000);
   it('respond with matching records', async function() {
     var client = new LibraClient("testnet", appkey);
-    console.log(client);
-    assert.equal(2.0, 2);
-    data = await client.getAccountBalance("000000000000000000000000000000000000000000000000000000000a550c18");
+    //console.log(client);
+    data = await client.addressAPI.getAccountBalance("000000000000000000000000000000000000000000000000000000000a550c18");
     assert(data.balance > 0)
+    console.log(data)
     return data;
   });
 });
