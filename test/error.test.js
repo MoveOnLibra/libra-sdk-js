eb9e8ea54269900a5a3577e1940334706bd67f00
yuan_xy <yuan_xin_yu@hotmail.com>
2019-12-25 20:27:42 +0800

    add client.parseError method with example

--- test/error.test.js
+++ test/error.test.js
@@ -27,4 +27,16 @@ describe('get balance should have address param', function () {
             }
         );
     });
+    it('test 400 response', async function () {
+        this.timeout(30000);
+        var client = new LibraClient("testnet", null, null);
+        client.apiClient.config.baseURL = "http://47.254.29.109/api";
+        try{
+            ret = await client.addressAPI.getAccountBalance("bad address");
+            assert.fail("not reachable.");
+        }catch(error){
+            assert.equal("400: address should be length 64 hex string, real len:11", client.parseError(error));
+        }
+        
+    });
 });
